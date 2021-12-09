6f0b29c4caf38ce62ae53ff1b40c70e47ac47e5d
Youfu Zhang <zhangyoufu@gmail.com>
2021-12-10 04:29:57 +0800

    remove JndiLookup.class

--- patch.sh
+++ patch.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 set -eu
-SCRIPT_DIR=$(cd -- $(dirname -- "$0"); pwd)
 for JAR_PATH in "$@"; do
 	if [[ $JAR_PATH != *log4j-core*.jar ]] || [ -e "$JAR_PATH.bak" ] || ! [ -f "$JAR_PATH" ]; then
 		echo "Skipped $JAR_PATH"
 		continue
 	fi
 	echo "Processing $JAR_PATH"
-	WORKSPACE=$(mktemp -d)
-	cp -p "$JAR_PATH" "$WORKSPACE/log4j-core.jar"
-	(
-		cd "$WORKSPACE"
-		unzip -q log4j-core.jar org/apache/logging/log4j/core/lookup/Interpolator.class
-		(
-			cd org/apache/logging/log4j/core/lookup
-			"$SCRIPT_DIR/Krakatau/disassemble.py" -out . -roundtrip Interpolator.class >/dev/null
-			sed -i -e 's|JndiLookup|NoMoreJndi|' Interpolator.j
-		)
-		"$SCRIPT_DIR/Krakatau/assemble.py" -out . org/apache/logging/log4j/core/lookup/Interpolator.j >/dev/null
-		zip -9fq log4j-core.jar org/apache/logging/log4j/core/lookup/Interpolator.class
-	)
-	mv "$JAR_PATH" "$JAR_PATH.bak"
-	mv "$WORKSPACE/log4j-core.jar" "$JAR_PATH"
-	rm -rf "$WORKSPACE"
+	cp -p "$JAR_PATH" "$JAR_PATH.bak"
+	zip -q -d "$JAR_PATH" org/apache/logging/log4j/core/lookup/JndiLookup.class
 done
